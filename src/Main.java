20669416c841e609fac124abf169815358a44b42
Vasiliy-Gorbunov <79896130110@yandex.ru>
2022-12-02 14:31:32 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int one = 1;
+        byte two = 2;
+        short three = 3;
+        long four = 4;
+        float five = 5f;
+        double six = 6L;
+        System.out.println("Значение переменной one с типом int равно "+one);
+        System.out.println("Значение переменной two с типом byte равно "+two);
+        System.out.println("Значение переменной three с типом short равно "+three);
+        System.out.println("Значение переменной four с типом long равно "+four);
+        System.out.println("Значение переменной five с типом float равно "+five);
+        System.out.println("Значение переменной six с типом double равно "+six);
+
+
+    }
+}
